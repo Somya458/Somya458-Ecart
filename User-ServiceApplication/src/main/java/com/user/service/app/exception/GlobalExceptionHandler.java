d344cfe00164d9fb46eae0a64ef926a8a3c2c32b
somya <somyapkb@gmail.com>
2024-08-20 22:37:44 +0530

    user-api

---
+++ User-ServiceApplication/src/main/java/com/user/service/app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.user.service.app.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // Other exception handlers can be added here
+}
---
+++ User-ServiceApplication/src/main/java/com/user/service/app/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.user.service.app.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
